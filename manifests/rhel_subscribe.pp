7df0300a25f5f7d62709706b330e11929a51afae
Paul Reed <paul.reed@puppet.com>
2022-04-29 16:10:20 -0400

    wrap in sensitive

--- manifests/rhel_subscribe.pp
+++ manifests/rhel_subscribe.pp
@@ -15,8 +15,8 @@ class utils::rhel_subscribe {
     and $facts['rhel_sub_info']['status']!='Subscribed'
     and $facts['rhel_sub_info']['status']!=''
   ) {
-    $rhel_user=lookup('rhel_subscription::user', undef, undef, '')
-    $rhel_pswd=lookup('rhel_subscription::password', undef, undef, '')
+    $rhel_user=Sensitive(lookup('rhel_subscription::user', undef, undef, ''))
+    $rhel_pswd=Sensitive(lookup('rhel_subscription::password', undef, undef, ''))
     unless (($rhel_user == '') or ($rhel_pswd == '')) {
       exec { "/usr/sbin/subscription-manager register --username=${rhel_user} --auto-attach --password='${rhel_pswd}'": }
     }
