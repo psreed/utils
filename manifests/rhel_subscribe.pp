698ab586159393f944bf57d5c3626cd2e0758399
Paul Reed <paul.reed@puppet.com>
2022-04-29 15:49:43 -0400

    add check for undef fact

--- manifests/rhel_subscribe.pp
+++ manifests/rhel_subscribe.pp
@@ -6,7 +6,8 @@
 #   include utils::rhel_subscribe
 class utils::rhel_subscribe {
   if (
-    $facts['os']['distro']['id']=='RedHatEnterpise'
+    $facts['os']['distro']['id']!=undef
+    and $facts['os']['distro']['id']=='RedHatEnterpise'
     and $facts['rhel_sub_info']['status']!='Subscribed'
     and $facts['rhel_sub_info']['status']!=''
   ) {
