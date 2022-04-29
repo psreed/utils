f1da55177597c88e31e096bf2a47246c0ecda242
Paul Reed <paul.reed@puppet.com>
2022-04-29 15:46:02 -0400

    add fact test

--- manifests/rhel_subscribe.pp
+++ manifests/rhel_subscribe.pp
@@ -5,9 +5,11 @@
 # @example
 #   include utils::rhel_subscribe
 class utils::rhel_subscribe {
-  $rhel_user=lookup('rhel_subscription::user', undef, undef, '')
-  $rhel_pswd=lookup('rhel_subscription::password', undef, undef, '')
-  unless (($rhel_user == '') or ($rhel_pswd == '')) {
-    exec { "/usr/sbin/subscription-manager register --username=${rhel_user} --auto-attach --password='${rhel_pswd}'": }
+  if ($facts['rhel_sub_info']['status']!='Subscribed' and $facts['rhel_sub_info']['status']!='') {
+    $rhel_user=lookup('rhel_subscription::user', undef, undef, '')
+    $rhel_pswd=lookup('rhel_subscription::password', undef, undef, '')
+    unless (($rhel_user == '') or ($rhel_pswd == '')) {
+      exec { "/usr/sbin/subscription-manager register --username=${rhel_user} --auto-attach --password='${rhel_pswd}'": }
+    }
   }
 }
