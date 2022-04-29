ddeca9fb4afea3c6dbadce57053b6005a9ec4225
Paul Reed <paul.reed@puppet.com>
2022-04-29 15:39:03 -0400

    swap from notify -> exec

--- manifests/rhel_subscribe.pp
+++ manifests/rhel_subscribe.pp
@@ -8,8 +8,5 @@ class utils::rhel_subscribe {
   $rhel_user=lookup('rhel_subscription::user', undef, undef, '')
   $rhel_pswd=lookup('rhel_subscription::password', undef, undef, '')
   unless (($rhel_user == '') or ($rhel_pswd == '')) {
-    #exec { "/usr/sbin/subscription-manager register --username=${rhel_user} --auto-attach --password='${rhel_pswd}'":
-    Notify { "/usr/sbin/subscription-manager register --username=${rhel_user} --auto-attach --password='${rhel_pswd}'": }
-  }
-
+    exec { "/usr/sbin/subscription-manager register --username=${rhel_user} --auto-attach --password='${rhel_pswd}'": }
 }
