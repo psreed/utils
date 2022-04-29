9003a54271a685be7f39028a81a15a79a8c54bb5
Paul Reed <paul.reed@puppet.com>
2022-04-29 15:47:59 -0400

    add test for RedHatEnterpise os fact

--- manifests/rhel_subscribe.pp
+++ manifests/rhel_subscribe.pp
@@ -5,7 +5,11 @@
 # @example
 #   include utils::rhel_subscribe
 class utils::rhel_subscribe {
-  if ($facts['rhel_sub_info']['status']!='Subscribed' and $facts['rhel_sub_info']['status']!='') {
+  if (
+    $facts['os']['distro']['id']=='RedHatEnterpise'
+    and $facts['rhel_sub_info']['status']!='Subscribed'
+    and $facts['rhel_sub_info']['status']!=''
+  ) {
     $rhel_user=lookup('rhel_subscription::user', undef, undef, '')
     $rhel_pswd=lookup('rhel_subscription::password', undef, undef, '')
     unless (($rhel_user == '') or ($rhel_pswd == '')) {
