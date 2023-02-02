4ab0afab7c41cfe598e847eb30c4edfa56168bcc
Paul Reed <paul.reed@puppet.com>
2023-02-02 12:20:32 -0500

    fix duplicate on RHEL 9.1

--- facts.d/rhel_sub_info.sh
+++ facts.d/rhel_sub_info.sh
@@ -4,7 +4,7 @@
 if [[ -f /etc/redhat-release && $(cat /etc/redhat-release) == *"Enterprise Linux"* ]]; then   
     OIFS=$IFS; IFS=;
     LIST=$(subscription-manager list)
-    STATUS=$(echo ${LIST} | grep "Status:" | awk '{print $2}')
+    STATUS=$(echo ${LIST} | grep "Red Hat Enterprise Linux for" -A 5 | grep "Status:" | awk '{print $2}')
     IFS=$OIFS
     echo "{\"rhel_sub_info\": { \"status\": \"${STATUS}\", \"info\": \"${LIST}\" } }"
 fi
