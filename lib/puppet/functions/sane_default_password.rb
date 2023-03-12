0e08d912cdc4c3ba35e18edde64dec1f211b820f
Paul Reed <paul.reed@puppet.com>
2023-03-12 15:00:21 -0400

    add puppet server function for sane default paswd

---
+++ lib/puppet/functions/sane_default_password.rb
@@ -0,0 +1,12 @@
+# Function creates a sane default password
+Puppet::Functions.create_function(:sane_default_password) do
+    dispatch :func do
+      optional_param 'Integer', :characters
+      return_type 'String'
+    end
+    def func(characters = 32)
+      cmd = "/opt/puppetlabs/sane_default_password_generator.sh #{characters}"
+      code = %x[ #{cmd} ]
+      code
+     end
+  end
