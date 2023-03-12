2c9632a9840c03f1244f6c7c0925d1d7d4527616
Paul Reed <paul.reed@puppet.com>
2023-03-12 14:47:55 -0400

    Sane Defaul Password Generator

---
+++ manifests/sane_default_password_generator.pp
@@ -0,0 +1,15 @@
+# intended to manage a file on PE infra servers to use with 
+# Puppet's generate command to create sane default passwords
+class utils::sane_default_password_generator {
+  file { '/opt/puppetlabs/sane_password_generate.sh':
+    ensure  => file,
+    owner   => 'pe_puppet',
+    group   => 'pe_puppet',
+    mode    => '0755',
+    content => '#!/bin/sh
+LENGTH=$1
+if [ "$LENGTH" == "" ]; then LENGTH=32; fi
+</dev/urandom tr -cd "[:graph:]" | head -c$LENGTH; echo ""
+',
+  }
+}
