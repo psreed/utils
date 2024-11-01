6aaa9f62145c360adf572596d5a4f4e24d2ce27d
Paul Reed <paul.reed@puppet.com>
2024-11-01 10:50:41 -0400

    Update subscription status fact

--- facts.d/rhel_sub_info.sh
+++ facts.d/rhel_sub_info.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #Add fact for RHEL subscription info/status
 
-if [[ -f /etc/redhat-release && $(cat /etc/redhat-release) == *"Enterprise Linux"* ]]; then   
+if [[ -f /etc/redhat-release && $(cat /etc/redhat-release) == *"Enterprise Linux"* ]]; then
     OIFS=$IFS; IFS=;
-    LIST=$(subscription-manager list)
-    STATUS=$(echo ${LIST} | grep "Red Hat Enterprise Linux for" -A 5 | grep "Status:" | awk '{print $2}')
+    STATUS=$(subscription-manager status)
+    PURPOSE=$(subscription-manager syspurpose)
+    SCA=$(echo $STATUS | grep "Content Access Mode is set to Simple Content Access.")
+    if [ $? == 0 ]; then
+            echo "{\"rhel_sub_info\": { \"status\": \"Subscribed\", \"info\": \"Simple Content Access \(SCA\) Enabled\", \"syspurpose\": ${PURPOSE} } }"
+    else
+            OSTATUS=$(echo ${STATUS} | grep "Overall Status:" | awk '{print $2}')
+            echo "{\"rhel_sub_info\": { \"status\": \"${OSTATUS}\", \"info\": \"${STATUS}\", \"syspurpose\": ${PURPOSE} } }"
+    fi
     IFS=$OIFS
-    echo "{\"rhel_sub_info\": { \"status\": \"${STATUS}\", \"info\": \"${LIST}\" } }"
 fi
