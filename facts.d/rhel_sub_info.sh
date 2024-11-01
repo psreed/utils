757fa44856eef522783ad1d9df337ebb8fddb730
Paul Reed <paul.reed@puppet.com>
2024-11-01 11:22:53 -0400

    Remove escapes around brackets

--- facts.d/rhel_sub_info.sh
+++ facts.d/rhel_sub_info.sh
@@ -7,7 +7,7 @@ if [[ -f /etc/redhat-release && $(cat /etc/redhat-release) == *"Enterprise Linux
     PURPOSE=$(subscription-manager syspurpose)
     SCA=$(echo $STATUS | grep "Content Access Mode is set to Simple Content Access.")
     if [ $? == 0 ]; then
-            echo "{\"rhel_sub_info\": { \"status\": \"Subscribed\", \"info\": \"Simple Content Access \(SCA\) Enabled\", \"syspurpose\": ${PURPOSE} } }"
+            echo "{\"rhel_sub_info\": { \"status\": \"Subscribed\", \"info\": \"Simple Content Access (SCA) Enabled\", \"syspurpose\": ${PURPOSE} } }"
     else
             OSTATUS=$(echo ${STATUS} | grep "Overall Status:" | awk '{print $3}')
             echo "{\"rhel_sub_info\": { \"status\": \"${OSTATUS}\", \"info\": \"${STATUS}\", \"syspurpose\": ${PURPOSE} } }"
