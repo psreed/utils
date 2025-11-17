9ffe5602f70831d173952aa9de2c75a2da13880a
Paul Reed <paul.reed@puppet.com>
2025-11-17 12:34:37 -0500

    Syntax updates for Rubocop

--- lib/puppet/functions/utils/generate_sane_default_password.rb
+++ lib/puppet/functions/utils/generate_sane_default_password.rb
@@ -1,12 +1,12 @@
 # Function creates a sane default password
 Puppet::Functions.create_function(:'utils::generate_sane_default_password') do
-    dispatch :func do
-      optional_param 'Integer', :characters
-      return_type 'String'
-    end
-    def func(characters = 32)
-      cmd = "/opt/puppetlabs/generate_sane_default_password.sh #{characters}"
-      code = %x[ #{cmd} ]
-      code
-     end
+  dispatch :func do
+    optional_param 'Integer', :characters
+    return_type 'String'
   end
+  def func(characters = 32)
+    cmd = "/opt/puppetlabs/generate_sane_default_password.sh #{characters}"
+    code = %x(`#{cmd}`)
+    code
+  end
+end
